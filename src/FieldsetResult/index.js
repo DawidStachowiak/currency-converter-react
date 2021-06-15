1a194a637c79b3ddc1d3a3bd3ef136b40150b2a7
Dawid Stachowiak <dawid@zetit.pl>
2021-06-15 17:27:31 +0200

    added result function

---
+++ src/FieldsetResult/index.js
@@ -0,0 +1,11 @@
+import "./style.css";
+import { Result } from "../Result";
+
+
+export const wynik =(result)=>{
+
+    <fieldset className="fieldset__result">
+        <legend>Po przeliczeniu</legend>
+            <Result result={result} />
+            </fieldset>
+}
