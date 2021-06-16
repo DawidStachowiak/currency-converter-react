0e2de127b85c2a981a44c6ef1fb38029f4eaa7e1
Dawid Stachowiak <dawid@zetit.pl>
2021-06-16 13:47:56 +0200

    added name and style to result div

--- src/FieldsetResult/index.js
+++ src/FieldsetResult/index.js
@@ -1,11 +1,12 @@
+import "./style.css";
 import { Result } from "../Form/Result";
 
 export const FieldsetResult = (result) => {
-  <fieldset class="fieldset__result">
+  <div class="result__box">
     <h1>Po przeliczeniu</h1>
 
-    <p class="formResult__paragraph js-result__paragraph">
+    <p class="formResult__paragraph">
       <Result result={result} />
     </p>
-  </fieldset>;
+  </div>;
 };
