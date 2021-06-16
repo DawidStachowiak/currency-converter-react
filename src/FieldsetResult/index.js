11b7ceb531490cca80e8563b46c4cc3511c4d299
Dawid Stachowiak <dawid@zetit.pl>
2021-06-16 13:52:02 +0200

    rename div result__box

--- src/FieldsetResult/index.js
+++ src/FieldsetResult/index.js
@@ -2,7 +2,7 @@ import "./style.css";
 import { Result } from "../Form/Result";
 
 export const FieldsetResult = (result) => {
-  <div class="result__box">
+  <div class="form__resultBox">
     <h1>Po przeliczeniu</h1>
 
     <p class="formResult__paragraph">
