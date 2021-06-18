84817a43ff5aba9271ad963be91988039e477d74
Dawid Stachowiak <dawid@zetit.pl>
2021-06-18 22:42:46 +0200

    useEffect added

--- src/Clock/index.js
+++ src/Clock/index.js
@@ -5,8 +5,16 @@ export const ShowDateAndTime = () => {
 
     const [time, setTime] = useState(0);
     const newDate = new Date();
-
-
+    const currentDate = newDate.toLocaleDateString(undefined, {
+        weekday: "long",
+        day: "numeric",
+        month: "long",
+        year: "numeric",
+        });
+        useEffect(() => {
+            const intervalId = setInterval(() => {
+            setTime((prevTime) => prevTime + 1);
+            }, 1000);
 
 };
 
