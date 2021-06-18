4d15ed9578c387bee0497a8acd50aee8f3d07456
Dawid Stachowiak <dawid@zetit.pl>
2021-06-18 22:25:23 +0200

    correct div class

--- src/Form/Result/index.js
+++ src/Form/Result/index.js
@@ -1,7 +1,7 @@
 import "./style.css";
 import React from "react";
 const Result = ({ result }) => (
-  <div class="form__resultBox">
+  <div className="form__resultBox">
     {result !== undefined && (
       <p className="form__resultParagraph">
         Wynik: &nbsp;{result.targetAmount.toFixed(2)}&nbsp;
