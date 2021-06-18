f3daf315779330fd0d334d932483ec936fbdab95
Dawid Stachowiak <dawid@zetit.pl>
2021-06-18 22:45:23 +0200

    cleaning function

--- src/Clock/index.js
+++ src/Clock/index.js
@@ -2,21 +2,20 @@ import "./style.css";
 import { useState, useEffect } from "react";
 
 export const ShowDateAndTime = () => {
-
-    const [time, setTime] = useState(0);
-    const newDate = new Date();
-    const currentDate = newDate.toLocaleDateString(undefined, {
-        weekday: "long",
-        day: "numeric",
-        month: "long",
-        year: "numeric",
-        });
-        useEffect(() => {
-            const intervalId = setInterval(() => {
-            setTime((prevTime) => prevTime + 1);
-            }, 1000);
-
+  const [time, setTime] = useState(0);
+  const newDate = new Date();
+  const currentDate = newDate.toLocaleDateString(undefined, {
+    weekday: "long",
+    day: "numeric",
+    month: "long",
+    year: "numeric",
+  });
+  useEffect(() => {
+    const intervalId = setInterval(() => {
+      setTime((prevTime) => prevTime + 1);
+    }, 1000);
+    return () => {
+      clearInterval(intervalId);
+    };
+  }, [time]);
 };
-
-
-
