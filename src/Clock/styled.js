b600f2f179e0ed3a452a2e11252432f9d77bfed1
Dawid Stachowiak <dawid@zetit.pl>
2021-07-01 20:47:08 +0200

    correct code

--- src/Clock/styled.js
+++ src/Clock/styled.js
@@ -1,8 +1,8 @@
 import styled from "styled-components";
 
 export const ClockContainer = styled.p`
-text-align: right;
+  text-align: right;
   color: #ffffff;
-  font-family: 'Roboto', sans-serif;
+  font-family: "Roboto", sans-serif;
   margin-right: 2px;
 `;
