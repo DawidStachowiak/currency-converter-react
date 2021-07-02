e6c80a3f943049c61999a961c4ab223a973665f3
Dawid Stachowiak <dawid@zetit.pl>
2021-07-02 23:38:27 +0200

    added colors to theme js

--- src/Form/styled.js
+++ src/Form/styled.js
@@ -1,15 +1,15 @@
 import styled from "styled-components";
 
 export const FormWrapper = styled.form`
-  color: rgb(71, 134, 250);
+  color: ${({ theme }) => theme.colors.dodgerBlue};
   text-align: center;
   width: 100%;
 `;
 
 export const FormInput = styled.input`
   padding: 10px;
-  background-color: rgb(241, 241, 238);
-  border-color: rgb(70, 130, 180);
+  background-color: ${({ theme }) => theme.colors.desertStorm};
+  border-color: ${({ theme }) => theme.colors.cornflowerBlue};
   max-width: 350px;
   border-radius: 4px;
   width: 100%;
@@ -18,21 +18,20 @@ export const FormInput = styled.input`
 `;
 
 export const FormFieldset = styled.fieldset`
-  border-color: rgb(30, 91, 224);
+  border-color: ${({theme})=>theme.colors.mariner};
   border-radius: 3px;
   margin-top: 15px;
   margin-bottom: 20px;
-  background-color: rgb(251, 255, 255);
+  background-color: ${({ theme }) => theme.colors.twilightBlue};
 `;
 
 export const FormLabel = styled.label`
   font-size: 18px;
-  color: hsl(0, 76%, 59%);
   display: inline-block;
   max-width: 300px;
   width: 100%;
   font-size: 18px;
-  color: midnightblue;
+  color: ${({ theme }) => theme.colors.midnightBlue};
   margin-left: 15px;
   margin-top: 10px;
   margin-right: 15px;
@@ -42,36 +41,35 @@ export const FormButton = styled.button`
   width: 100%;
   border: none;
   padding: 15px;
-  background-color: rgb(70, 130, 180);
-  color: hsl(0, 0%, 96%);
+  background-color: ${({ theme }) => theme.colors.steelBlue};
+  color: ${({ theme }) => theme.colors.wildSand};
   margin-top: 15px;
   font-family: "Lato", sans-serif;
 
-  &:hover{
-    background-color: rgb(69, 132, 248);
+  &:hover {
+    background-color: ${({ theme }) => theme.colors.indigo};
   }
 `;
 
 export const FormLegend = styled.legend`
-background-color: rgb(70, 130, 180);
+  background-color: ${({ theme }) => theme.colors.blackOlive};
   border-radius: 3px;
   padding: 5px;
-  border: 1px solid rgb(70, 130, 180);
+  border: 1px solid ${({ theme }) => theme.colors.steelBlue};
   font-size: 18px;
-  color: hsl(0, 0%, 96%);
+  color: ${({ theme }) => theme.colors.wildSand};
   font-family: "Lato", sans-serif;
 `;
 
 export const FormSelect = styled.select`
-padding: 10px;
-background-color: rgb(70, 130, 180);
-border-color: rgb(30, 91, 224);
-max-width: 350px;
-border-radius: 4px;
-width: 100%;
-margin-top: 20px;
-margin-bottom: 20px;
-color: hsl(0, 0%, 96%);
-font-family: "Lato", sans-serif;
-`
-;
+  padding: 10px;
+  background-color: ${({ theme }) => theme.colors.steelBlue};
+  border-color: ${({theme})=>theme.colors.mariner};
+  max-width: 350px;
+  border-radius: 4px;
+  width: 100%;
+  margin-top: 20px;
+  margin-bottom: 20px;
+  color: ${({ theme }) => theme.colors.wildSand};
+  font-family: "Lato", sans-serif;
+`;
--- src/theme.js
+++ src/theme.js
@@ -1,5 +1,15 @@
 export const theme = {
   colors: {
-    white: "hsl(0, 0%, 100%)",
-  },
+    white: "#1F0000",
+    dodgerBlue: "#4786FA",
+    desertStorm: "#F1F1EE",
+    cornflowerBlue: "#4682EE",
+    twilightBlue: "#FBFFFF",
+    midnightBlue: "#191970",
+    steelBlue: "#4682B4",
+    wildSand: "#F5F5F5",
+    blackOlive: "#242E16",
+    indigo: "#4682C8",
+    mariner: "#1E5BE0",
+  }
 };
